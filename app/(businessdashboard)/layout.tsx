1c99ef288c1657b3b36bf786098c76ed479e4871
Felix Kiprotich <felixkiprotich2000@gmail.com>
2026-07-16 09:28:49 -0600

    adjusted the dashboard

--- app/(businessdashboard)/layout.tsx
+++ app/(businessdashboard)/layout.tsx
@@ -38,7 +38,7 @@ const Layout = ({ children }: LayoutProps) => {
       )}
 
       {/* Main Area */}
-      <div className="items-center justify-center lg:ml-20 w-full">
+      <div className="items-center justify-center  min-h-screen flex flex-col lg:ml-20 w-full">
         {/* Top Navbar */}
         <div className="left-0 right-0 top-0 z-40 ">
           <TopNavBar
